5cfe913bb09175e79280eef691ed6e900676386b
Mieloch, Krzysztof <krzysztof.mieloch@lgln.niedersachsen.de>
2025-01-06 16:11:23 +0100

    NamedTupleFactory

--- polyfactory/factories/__init__.py
+++ polyfactory/factories/__init__.py
@@ -1,5 +1,6 @@
 from polyfactory.factories.base import BaseFactory
 from polyfactory.factories.dataclass_factory import DataclassFactory
+from polyfactory.factories.named_tuple_factory import NamedTupleFactory
 from polyfactory.factories.typed_dict_factory import TypedDictFactory
 
-__all__ = ("BaseFactory", "DataclassFactory", "TypedDictFactory")
+__all__ = ( "BaseFactory", "DataclassFactory", "NamedTupleFactory", "TypedDictFactory" )
